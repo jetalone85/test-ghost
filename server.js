0e08b952085a20f815cbf0138b80dfadf286f0b6
Marcin Pyrka <marcin.pyrka@polcode.net>
2018-09-05 00:46:54 +0200

    update

--- server.js
+++ server.js
@@ -1,27 +1,6 @@
 const express = require('express')
 const app = express()
 
-var shouldRun = true;
-var desiredLoadFactor = .5;
-
-function blockCpuFor(ms) {
-	var now = new Date().getTime();
-	var result = 0
-	while(shouldRun) {
-		result += Math.random() * Math.random();
-		if (new Date().getTime() > now +ms)
-			return;
-	}	
-}
-
-function start() {
-	shouldRun = true;
-	blockCpuFor(1000*desiredLoadFactor);
-	setTimeout(start, 1000* (1 - desiredLoadFactor));
-
-    return 0;
-}
-
-app.get('/', (req, res) => res.send('Hello World!!! Pańcia jest malutka!' + start()))
+app.get('/', (req, res) => res.send('Hello World!!! Pańcia jest malutka!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
