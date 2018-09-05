4b812dba43c5625eab62de22eb2bec241c7e56e3
Marcin Pyrka <marcin.pyrka@polcode.net>
2018-09-05 14:40:59 +0200

    update

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(morgan('combined'))
+
+function errorHandler (err, req, res, next) {
+    if (res.headersSent) {
+      return next(err)
+    }
+    res.status(500)
+    res.render('error', { error: err })
+  }
+
+app.use('/', express.static('public'))
+
 app.get('/healthz', (req, res) => res.send('ok!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
