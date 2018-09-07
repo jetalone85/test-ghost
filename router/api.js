6e7ed1af85d4c44d74aba03cf42b4881bc9ef7e7
Marcin Pyrka <marcin.pyrka@polcode.net>
2018-09-07 13:25:19 +0200

    update

--- router/api.js
+++ router/api.js
@@ -22,43 +22,127 @@ router.get('/character', function(req, res) {
 router.get('/abilities', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send(
-        JSON.stringify([{
-            name: 'S',
-            value1: 16,
-            value2: null,
-            value3: 20,
-            value4: null,
-        }, {
-            name: 'ZR',
-            value1: 22,
-            value2: null,
-            value3: null,
-            value4: null,
-        }, {
-            name: 'BD',
-            value1: 16,
-            value2: null,
-            value3: null,
-            value4: null,
-        }, {
-            name: 'INT',
-            value1: 22,
-            value2: null,
-            value3: null,
-            value4: null,
-        }, {
-            name: 'RZT',
-            value1: 26,
-            value2: null,
-            value3: null,
-            value4: null,
-        }, {
-            name: 'CHA',
-            value1: 22,
-            value2: null,
-            value3: null,
-            value4: null,
-        }])
+        JSON.stringify(
+            [{
+                name: 'S',
+                value1: 16,
+                value2: null,
+                value3: 20,
+                value4: null,
+            }, {
+                name: 'ZR',
+                value1: 22,
+                value2: null,
+                value3: null,
+                value4: null,
+            }, {
+                name: 'BD',
+                value1: 16,
+                value2: null,
+                value3: null,
+                value4: null,
+            }, {
+                name: 'INT',
+                value1: 22,
+                value2: null,
+                value3: null,
+                value4: null,
+            }, {
+                name: 'RZT',
+                value1: 26,
+                value2: null,
+                value3: null,
+                value4: null,
+            }, {
+                name: 'CHA',
+                value1: 22,
+                value2: null,
+                value3: null,
+                value4: null,
+            }]
+        )
+    );
+})
+
+
+router.get('/skills', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(
+        JSON.stringify(
+            [{
+                    name: 'Blefowanie',
+                    value1: null,
+                    value2: null,
+                    value3: 12,
+                    value4: null,
+                    ability: 'CHA',
+                },
+                {
+                    name: 'Ciche poruszanie',
+                    value1: null,
+                    value2: null,
+                    value3: 15,
+                    value4: 4,
+                    ability: 'ZR',
+                },
+                {
+                    name: 'Czarostwo',
+                    value1: null,
+                    value2: null,
+                    value3: 12,
+                    value4: 2,
+                    ability: 'INT',
+                },
+                {
+                    name: 'Dyplomacja',
+                    value1: null,
+                    value2: null,
+                    value3: 8,
+                    value4: 0,
+                    ability: 'CHA',
+                },
+                {
+                    name: 'Fałszerstwo',
+                    value1: null,
+                    value2: null,
+                    value3: 0,
+                    value4: 4,
+                    ability: 'INT',
+                },
+                {
+                    name: 'Jeździectwo',
+                    value1: null,
+                    value2: null,
+                    value3: 6,
+                    value4: 2,
+                    ability: 'ZR',
+                },
+                {
+                    name: 'Koncentracja',
+                    value1: null,
+                    value2: null,
+                    value3: 15,
+                    value4: 10,
+                    ability: 'BD',
+                },
+                {
+                    name: 'Leczenie',
+                    value1: null,
+                    value2: null,
+                    value3: 15,
+                    value4: 2,
+                    ability: 'RZT',
+                },
+                {
+                    name: 'Nasłuchiwanie',
+                    value1: null,
+                    value2: null,
+                    value3: 8,
+                    value4: 4,
+                    ability: 'RZT',
+                },
+            ]
+        )
     );
 })
 
