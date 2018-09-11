d27fe686a1c03505ca79066169d609d847dd6c60
Marcin Pyrka <marcin.pyrka@polcode.net>
2018-09-11 17:40:24 +0200

    update

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,8 +1,12 @@
 'use strict';
 
 const express = require('express')
+const path = require('path');
+
+var dsConfig = require('../datasources.json');
 
 module.exports = function(app) {
+    var User = app.models.user
     app.set('view engine', 'pug')
 
     app.use('/robots.txt', express.static('public/robots.txt'))
@@ -12,6 +16,67 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('index', {})
     })
+
+    //login page
+    app.get('/login', function(req, res) {
+        var credentials = dsConfig.emailDs.transports[0].auth;
+        res.render('login', {
+            email: credentials.user,
+            password: credentials.pass
+        });
+    });
+
+    //log a user in
+    app.post('/login', function(req, res) {
+        User.login({
+            email: req.body.email,
+            password: req.body.password
+        }, 'user', function(err, token) {
+            if (err) {
+                if (err.details && err.code === 'LOGIN_FAILED_EMAIL_NOT_VERIFIED') {
+                    res.render('reponseToTriggerEmail', {
+                        title: 'Login failed',
+                        content: err,
+                        redirectToEmail: '/api/users/' + err.details.userId + '/verify',
+                        redirectTo: '/',
+                        redirectToLinkText: 'Click here',
+                        userId: err.details.userId
+                    });
+                } else {
+                    res.render('response', {
+                        title: 'Login failed. Wrong username or password',
+                        content: err,
+                        redirectTo: '/',
+                        redirectToLinkText: 'Please login again',
+                    });
+                }
+                return;
+            }
+            res.render('home', {
+                email: req.body.email,
+                accessToken: token.id,
+                redirectUrl: '/api/users/change-password?access_token=' + token.id
+            });
+        });
+    });
+
+    //verified
+    app.get('/verified', function(req, res) {
+        res.render('verified');
+    });
+
+    app.get('/logout', function(req, res, next) {
+        if (!req.accessToken) return res.sendStatus(401);
+        User.logout(req.accessToken.id, function(err) {
+            if (err) return next(err);
+            res.redirect('/');
+        });
+    });
+
+    //verified
+    app.get('/verified', function(req, res) {
+        res.render('verified');
+    });
 
     app.get('/sheet/:CharacterId', function(req, res) {
 
